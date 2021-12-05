60108fe99b5b4375d4c4d2cee28b4e82d04adbf8
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2021-12-06 00:06:46 +0700

    Refactor code edit profile page

--- components/organisms/Sidebar/index.tsx
+++ components/organisms/Sidebar/index.tsx
@@ -2,18 +2,22 @@ import Footer from './Footer';
 import MenuItem from './MenuItem';
 import Profile from './Profile';
 
-export default function Sidebar() {
+interface SidebarProps{
+  activeMenu: 'overview'|'transactions' |'settings'
+}
+export default function Sidebar(props:SidebarProps) {
+  const { activeMenu } = props;
   return (
     <section className="sidebar">
       <div className="content pt-50 pb-30 ps-30">
         <Profile />
         <div className="menus">
-          <MenuItem title="Overview" icon="ic-menu-overview" active href="/member" />
-          <MenuItem title="Transactions" icon="ic-menu-transactions" href="/member/transactions" />
+          <MenuItem title="Overview" icon="ic-menu-overview" active={activeMenu === 'overview'} href="/member" />
+          <MenuItem title="Transactions" icon="ic-menu-transactions" active={activeMenu === 'transactions'} href="/member/transactions" />
           <MenuItem title="Messages" icon="ic-menu-messages" href="/member" />
           <MenuItem title="Card" icon="ic-menu-card" href="/member" />
           <MenuItem title="Rewards" icon="ic-menu-rewards" href="/member" />
-          <MenuItem title="Settings" icon="ic-menu-settings" href="/member/edit-profile" />
+          <MenuItem title="Settings" icon="ic-menu-settings" active={activeMenu === 'settings'} href="/member/edit-profile" />
           <MenuItem title="Log Out" icon="ic-menu-logout" href="/sign-in" />
         </div>
         <Footer />
