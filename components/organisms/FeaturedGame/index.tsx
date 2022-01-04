03ec92e8c6b6a9aee17ee7ddbd8122da4c4f380e
Muhamad Fahmi Rahmatullah <fahmi.rahmat07@gmail.com>
2022-01-04 13:28:58 +0700

    Config API services and data-types, then setup route dinamis for voucher detail and integrated it. Setup nominal and payment component on voucher detail

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { GameItemTypes } from '../../../services/data-types';
+import { getFeaturedGame } from '../../../services/player';
 import GameItem from '../../molecules/GameItem';
-import axios from 'axios'
 
 export default function FeaturedGame() {
+  const [gameList, setGamelist] = useState([]);
 
-  const [gameList, setGamelist]= useState([]);
+  const getFeatureGameList = useCallback(async () => {
+    const data = await getFeaturedGame();
+    setGamelist(data);
+  }, [getFeaturedGame]);
 
-  useEffect(async () => {
-    const response = await axios.get('https://store-gg073.herokuapp.com/api/v1/players/landingpage');
-    console.log('data: ',response.data)
-    setGamelist(response.data.data)
-  }, [])
+  useEffect(() => {
+    getFeatureGameList();
+  }, []);
+
+  const API_IMG = process.env.NEXT_PUBLIC_IMG;
 
   return (
     <section className="featured-game pt-50 pb-50">
@@ -25,12 +30,9 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          {gameList.map(item => {
-            return (
-              <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://store-gg073.herokuapp.com/uploads/${item.thumbnail}`} />
-            )
-          })}
-
+          {gameList.map((item:GameItemTypes) => (
+            <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`${API_IMG}/${item.thumbnail}`} id={item._id} />
+          ))}
 
         </div>
       </div>
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -1,9 +1,35 @@
+import { useRouter } from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
 import Footer from '../../components/organisms/Footer';
 import Navbar from '../../components/organisms/Navbar';
 import TopUpForm from '../../components/organisms/TopUpForm';
 import TopUpItem from '../../components/organisms/TopUpItem';
+import { getDetailVoucher } from '../../services/player';
 
 export default function Detail() {
+  const {query, isReady} = useRouter();
+  const [dataItem, setDataItem] = useState({
+    name: '',
+    thumbnail: '',
+    category:{
+      name: ''
+    }
+  })
+
+  const getVoucherDetailAPI = useCallback(async (id)=>{
+    const data = await getDetailVoucher(id);
+    setDataItem(data);
+    console.log('data: ',data);
+  },[])
+
+  useEffect(() => {
+    if(isReady){
+      getVoucherDetailAPI(query.id);
+    }else{
+      console.log('router tida tersedia');
+    }
+  }, [isReady])
+
   return (
     <>
       <Navbar />
@@ -15,10 +41,10 @@ export default function Detail() {
           </div>
           <div className="row">
             <div className="col-xl-3 col-lg-4 col-md-5 pb-30 pb-md-0 pe-md-25 text-md-start">
-              <TopUpItem type="mobile" />
+              <TopUpItem type="mobile" data={dataItem} />
             </div>
             <div className="col-xl-9 col-lg-8 col-md-7 ps-md-25">
-              <TopUpItem type="dekstop" />
+              <TopUpItem type="dekstop" data={dataItem} />
               <hr />
               <TopUpForm />
             </div>
